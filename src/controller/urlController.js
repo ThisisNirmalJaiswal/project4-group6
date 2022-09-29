6ca3f3f7792c0c0e525c014f6f16a75b9cc4c403
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2022-09-29 16:59:33 +0530

    getapi

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,98 +1,60 @@
 var validUrl = require('valid-url');
-
 const urlmodel = require("../models/urlShortnerModel")
-
 const shortid = require('shortid');
-
 const isValid = function (value) {
-
     if (typeof value === "undefined" || value === null) return false
-
     if (typeof value === "string" && value.trim().length === 0) return false
-
     return true
-
 }
 
 const createUrl = async function (req, res) {
-
     try {
-
        if(!Object.keys(req.body).length>0){
-
            return res.status(400).send({status:false, message:"Please provide some data in body" })
-
        }
-
        let {longUrl} = req.body
-
        if(!isValid(longUrl)){
-
         return res.status(400).send({status:false,message:"longUrl is required"})
-
        }
-
        if (!validUrl.isUri(longUrl)){
-
         return res.status(400).send({status:false,message:"invalid url"})
-
     } 
-
     let findurl = await urlmodel.findOne({longUrl:longUrl})
-
     if(findurl){
-
-        return res.status(400).sens({status:false, message:"url already present"})
-
+        return res.status(400).send({status:false, message:"url already present"})
     }
-
     const urlCode = shortid.generate()
-
    const savedata = {
-
     "longUrl":longUrl,
-
     "urlCode":urlCode,
-
     "shortUrl":`http://${req.get("host")}/${urlCode}`
-
    }
-
    const createData = await urlmodel.create(savedata)
-
        return res.status(201).send({status:true, data : createData})
-
-    
-
     } catch (error) {
-
         return res.status(500).send({ status: false, error: error.message })
-
     }
-
 }
 
 const getUrl = async function (req,res){
 try{
-const shortCode = req.params.urlCode
+const urlCode = req.params.urlCode
 
-if (!shortCode) return res.status(400).send({status: false, msg : "please provide shortlink"})
+if (!urlCode) return res.status(400).send({status: false, msg : "please provide shortcode"})
 
-let shortModel = await urlmodel.findOne({urlCode:shortCode}).select({longUrl:1})
+let shortModel = await urlmodel.findOne({urlCode:urlCode}).select({_id:0,longUrl:1})
 
 if(!shortModel)
 return res.status(400).send({status : false,msg:"link not found"})
 
-return res.redirect(shortModel)
+return res.status(302).redirect(shortModel.longUrl)
 
 } catch(error) {
     return res.status(500).send({status:false,msg:error.message})
   
 }
 }
+
 module.exports.getUrl = getUrl;
-
-
-
 
 module.exports.createUrl=createUrl;
