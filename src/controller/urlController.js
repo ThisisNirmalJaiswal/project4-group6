5d29bd59004cfdaa96bf8b4f97a2308557fce082
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2022-10-07 12:13:24 +0530

    final done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,7 +1,6 @@
 const urlModel = require("../models/urlShortnerModel");
 const shortid = require("shortid");
 const redis = require("redis");
-const Axios = require('axios'); 
 const { promisify } = require("util");
 
 
@@ -65,24 +64,18 @@ const createUrl = async function (req, res) {
       return res.status(400).send({ status: false, message: "longUrl is required" });
     };
 
-
     let url = /^www\.[a-z0-9-]+(?:\.[a-z0-9-]+)*\.+(\w)*/
 
     if(url.test(longUrl)){
-      longUrl="http://"+longUrl
+      longUrl="https://"+longUrl
     }
 
-
-    let option = {
-      method: "get",
-      url: longUrl
+    const isValidUrl = (urlString) => {
+      return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(urlString)
+    }
+  if(!isValidUrl(longUrl)){
+    return res.status(400).send({status: false, message: "longUrl is not valid"})
   }
-
-  let exist = await Axios(option)
-      .then(() => longUrl)
-      .catch(() => null)
-
-  if (!exist) return res.status(400).send({ status: false, message: "url is not valid" })
 
 
     let cache = await GET_ASYNC(`${longUrl}`);
